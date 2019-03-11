e5512b5a770d1ba59cc4e07571b78f8f0396640b
Pedro S <pedrocarlos.ti@gmail.com>
2019-03-11 00:12:03 -0300

    Add Reactotron and RealmDB simple Schema

--- App.js
+++ App.js
@@ -7,25 +7,42 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, ScrollView } from "react-native";
+const Realm = require("realm");
+import "./src/config/ReactotronConfig";
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { realm: null };
+  }
+
+  componentWillMount() {
+    Realm.open({
+      schema: [{ name: "Dog", properties: { name: "string" } }]
+    }).then(realm => {
+      realm.write(() => {
+        realm.create("Dog", { name: "Rex" });
+      });
+      this.setState({ realm });
+      // let data = JSON.stringify(realm.objects("Dog"));
+      // data = JSON.parse(data);
+      // console.log(data);
+      console.tron.log(realm.objects("Dog"));
+    });
+  }
+
   render() {
+    const info = this.state.realm
+      ? this.state.realm.objects("Dog")
+      : "Loading...";
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ScrollView>
+        <Text style={styles.welcome}>{JSON.stringify(info)}</Text>
+      </ScrollView>
     );
   }
 }
@@ -33,18 +50,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
